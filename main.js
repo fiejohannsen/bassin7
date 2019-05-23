e85ac4ef961fc64abe027c4a098f7618919d9120
Fie Mølgaard  Johannsen <fiejohannsen@live.dk>
2019-05-23 14:29:12 +0200

    undersidekode

--- main.js
+++ main.js
@@ -15,3 +15,11 @@ console.log("forbindelse")
         }
       });
     }
+    function myFunction() {
+      var x = document.getElementById("myLinks");
+      if (x.style.display === "block") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      }
+    }
