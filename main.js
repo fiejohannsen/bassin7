57b0dfed9e7a479e78709efee038b7f883d0ab3c
Fie Mølgaard  Johannsen <fiejohannsen@live.dk>
2019-05-23 16:03:40 +0200

    collapsiblesider færdig

--- main.js
+++ main.js
@@ -15,11 +15,3 @@ console.log("forbindelse")
         }
       });
     }
-    function myFunction() {
-      var x = document.getElementById("myLinks");
-      if (x.style.display === "block") {
-        x.style.display = "none";
-      } else {
-        x.style.display = "block";
-      }
-    }
